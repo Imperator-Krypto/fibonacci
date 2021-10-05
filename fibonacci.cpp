9a72c87923c9c11d682960a4dec2b48830e62ff1
zet <59450620+zet-sec@users.noreply.github.com>
2021-10-05 13:32:00 +0300

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,4 +1,3 @@
-#include <cassert>
 #include <iostream>
 
 uint64_t fibonacci(uint64_t n) {
@@ -7,19 +6,9 @@ uint64_t fibonacci(uint64_t n) {
     }
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
-static void test() {
-    uint64_t test_case_1 = fibonacci(0);
-    assert(test_case_1 == 0);
-    std::cout << "Passed Test 1!" << std::endl;
-
-    uint64_t test_case_2 = fibonacci(1);
-    assert(test_case_2 == 1);
-    std::cout << "Passed Test 2!" << std::endl;
-}
-
 int main() {
-    test();
     int n = 0;
+    
     std::cin >> n;
     assert(n >= 0);
     std::cout << "F(" << n << ")= " << fibonacci(n) << std::endl;
